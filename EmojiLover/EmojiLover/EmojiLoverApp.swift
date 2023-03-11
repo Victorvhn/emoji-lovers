8c300630dcf747f8f226e6c8343ee06cd30ba3d3
Victor Hugo do Nascimento <victor.hugo.nascimento58@gmail.com>
2023-03-11 22:41:38 +1000

    Commit

---
+++ EmojiLover/EmojiLover/EmojiLoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoverApp.swift
+//  EmojiLover
+//
+//  Created by Victor Hugo do Nascimento on 11/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct EmojiLoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
